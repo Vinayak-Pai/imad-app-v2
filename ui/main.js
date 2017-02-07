aa3f92ae3ae826d7ea734d73d26c7d15abaed7ec
Vinayak-Pai <vinnu1pai@gmail.com>
2017-02-07 21:16:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 
 // modifying value using id
-var element= document.getElementById(main-text);
+var element= document.getElementById('main-text');
 
 element.innerHTML="The power of positive thinking"

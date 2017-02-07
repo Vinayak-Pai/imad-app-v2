e41e16832f73159ca6877ea86e678e4b1aba1019
Vinayak-Pai <vinnu1pai@gmail.com>
2017-02-07 21:32:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,12 @@ element.innerHTML="The power of positive thinking";
 
 //move the image by id
 var img=document.getElementById('image');
+var marginLeft = 0 ;
+function moveRight(){
+    marginLeft =  marginLeft + 10 ;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onclick = function (){
-    img.style.marginLeft = '100px';
+    var interval= setInterval(moveRight, 100);
+    
     };

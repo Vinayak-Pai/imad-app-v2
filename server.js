5654764aa72cf076ed8993b3bc8e910094b74aa5
Vinayak-Pai <vinnu1pai@gmail.com>
2017-02-08 20:48:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,7 +95,11 @@ app.get('/:articleName',function(req,res){
    res.send(createTemplate(articles[articleName]));
 });
 
-
+var count=0;
+app.get('/counter',function(req,res){
+    counter=counter+1;
+    res.send(counter.toString());
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));

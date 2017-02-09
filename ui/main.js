49aaa04b4ec2c74b4f9914cffda8873a5ddae69c
Vinayak-Pai <vinnu1pai@gmail.com>
2017-02-09 21:02:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -52,7 +52,7 @@ submit.onclick=function(){
     //make a request to server and send name
     // capture it as list
     var names = ["name1","name2","name3","name4"];
-    var list="";
+    var list='';
     for (var i=0; i < names.length; i++){
         list=='<li>'+ names[i]+ '</li>';
     }

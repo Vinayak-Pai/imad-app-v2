f767d7980a602714fc2e05bc96f1e8c9a1039c9f
Vinayak-Pai <vinnu1pai@gmail.com>
2017-03-05 11:23:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -56,63 +56,63 @@ submit.onclick=function(){
 
 
 
-var button= document.getElementById('counter');
+// var button= document.getElementById('counter');
 
-button.onclick = function(){
+// button.onclick = function(){
     
-    // create a request object to counter end point
-    var request = new XMLHttpRequest();
+//     // create a request object to counter end point
+//     var request = new XMLHttpRequest();
     
-    //capture the response and store in variable
-    request.onreadystatechange = function(){
-        if (request.readyState=== XMLHttpRequest.DONE){
-           if (request.status===200)
-           {
-               var counter=request.responseText;
-               var span=document.getElementById('count');
-               span.innerHTML= counter.toString();
-           }
-        }
-    };
-        //make a request
-        request.open('GET','http://vinayak-pai.imad.hasura-app.io/counter',true);
-        request.send(null);
-        };
+//     //capture the response and store in variable
+//     request.onreadystatechange = function(){
+//         if (request.readyState=== XMLHttpRequest.DONE){
+//           if (request.status===200)
+//           {
+//               var counter=request.responseText;
+//               var span=document.getElementById('count');
+//               span.innerHTML= counter.toString();
+//           }
+//         }
+//     };
+//         //make a request
+//         request.open('GET','http://vinayak-pai.imad.hasura-app.io/counter',true);
+//         request.send(null);
+//         };
         
 
 
-var submit = document.getElementById("submit_btn");
-submit.onclick=function(){
-    //make a request to server and send name
-     var request = new XMLHttpRequest();
+// var submit = document.getElementById("submit_btn");
+// submit.onclick=function(){
+//     //make a request to server and send name
+//      var request = new XMLHttpRequest();
     
-    //capture the response and store in variable
-    request.onreadystatechange = function(){
-        if (request.readyState=== XMLHttpRequest.DONE){
-           if (request.status===200)
-           {
-            var names = request.responseText;
-            names= JSON.parse(names);
-            var list='';
-            for (var i=0; i < names.length; i++){
-            list +='<li>'+ names[i]+ '</li>';
-            }
-            var ul =document.getElementById("namelist");
-            ul.innerHTML=list;
+//     //capture the response and store in variable
+//     request.onreadystatechange = function(){
+//         if (request.readyState=== XMLHttpRequest.DONE){
+//           if (request.status===200)
+//           {
+//             var names = request.responseText;
+//             names= JSON.parse(names);
+//             var list='';
+//             for (var i=0; i < names.length; i++){
+//             list +='<li>'+ names[i]+ '</li>';
+//             }
+//             var ul =document.getElementById("namelist");
+//             ul.innerHTML=list;
                
-           }
-        }
-    };
-        //make a request
-        var nameInput= document.getElementById("name");
-        var name= nameInput.value;
-        request.open('GET','http://vinayak-pai.imad.hasura-app.io/submit-name?name='+ name, true);
-        request.send(null);
+//           }
+//         }
+//     };
+//         //make a request
+//         var nameInput= document.getElementById("name");
+//         var name= nameInput.value;
+//         request.open('GET','http://vinayak-pai.imad.hasura-app.io/submit-name?name='+ name, true);
+//         request.send(null);
         
         
-    // capture it as list
+//     // capture it as list
    
-};
+// };
     
     
 
